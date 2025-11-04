11610fc3e0819009a59a4abcb8788c478272d2c7
Atuan0510 <phamtuan05102003@gmail.com>
2025-11-04 15:11:14 +0700

    123

--- FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8e8e127eb4292163ac9434daf29f9bc37be56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0e70c09dc033d6621d7751b8158bfea60fb730")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8e8e127eb4292163ac9434daf29f9bc37be56e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0e70c09dc033d6621d7751b8158bfea60fb730")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
